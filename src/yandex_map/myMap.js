643a9d63dd934b1203f99881ca381659cb05a0ca
nailoooon <97839003+nailoooon@users.noreply.github.com>
2023-02-04 20:08:23 +0600

    Update myMap.js

--- src/yandex_map/myMap.js
+++ src/yandex_map/myMap.js
@@ -9,7 +9,13 @@ import {FiMapPin} from "react-icons/fi";
 const MyMap = () => {
     return (
         <div className={styles.map}>
-            <MapContainer style={{height: "60vh", width: "60vw"}} center={[51.111192, 71.41304]} zoom={14.5}
+            <div className={styles.map__title}>
+                Где мы<span style={{color: "#3caaf7"}}> находимся</span>
+            </div>
+            <div className={styles.map__divider}>
+                ______
+            </div>
+            <MapContainer style={{height: "80vh", width: "80vw"}} center={[51.111192, 71.41304]} zoom={14.5}
                           scrollWheelZoom={true}>
                 <TileLayer
                     url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
@@ -24,22 +30,6 @@ const MyMap = () => {
                 />
                 ))}
             </MapContainer>
-            <div className={styles.map__card}>
-                <div className={styles.map__title}>
-                    Контакты
-                </div>
-                <div className={styles.map__sub}>
-                    <NavLink href="tel: +7 702 920 6161" style={{color: "black", marginBottom: "12px"}}>
-                        <BsFillTelephoneFill style={{color: "#3caaf7"}}/> +7 702 920 6161
-                    </NavLink>
-                    <NavLink href="https://api.whatsapp.com/send?phone=77029206161" style={{color: "black", marginBottom: "12px"}}>
-                        <BsWhatsapp style={{color: "#3caaf7"}}/> Написать на WhatsApp
-                    </NavLink>
-                    <NavLink href={"https://go.2gis.com/xbns1"}>
-                        <FiMapPin style={{color: "#3caaf7", fontSize: "large"}}/> Проспект Кабанбай батыр, 48/7
-                    </NavLink>
-                </div>
-            </div>
         </div>
     );
 };
